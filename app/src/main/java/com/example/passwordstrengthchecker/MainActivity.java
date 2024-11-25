95d045c17d9617dfa4753b41c8f8c0e3a3d168af
Moeez Umer <bscs23150@itu.edu.pk>
2024-11-25 16:47:11 +0500

    Boiler plate code for building our application(Password Strength Checker)

---
+++ app/src/main/java/com/example/passwordstrengthchecker/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.passwordstrengthchecker;
+
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+}
